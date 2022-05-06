293fab82ca2c5a0c5fa1371f3326927edaa47dfc
hamster <hamster@hamster.com>
2022-05-06 16:51:35 +0900

    다시원래대로

--- src/main/java/com/example/demo/board/controller/BoardController.java
+++ src/main/java/com/example/demo/board/controller/BoardController.java
@@ -23,7 +23,6 @@ public class BoardController {
         boardService.BoardSee(page,pagesize,model);
 
         log.info("model = {} ",model);
-        //test//test//test
         return "board";
     }
 
